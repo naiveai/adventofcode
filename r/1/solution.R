d7bbf7647707063ef8fe1c302aa933287ec4a7a9
Eshan <eshansingh@gmail.com>
2023-12-01 04:15:59 -0500

    Solve 2023-1 in R horribly badly

---
+++ r/1/solution.R
@@ -0,0 +1,36 @@
+library(plyr)
+library(tidyverse)
+
+basic_calibration_number <- function(x) {
+    only_numeric <- gsub("[^0-9.-]", "", x)
+    two_digit_number_str <- paste(
+        substr(only_numeric, 1, 1),
+        substr(only_numeric, nchar(only_numeric), nchar(only_numeric)),
+        sep=""
+    )
+    as.numeric(two_digit_number_str)
+}
+
+calibration_number <- function(x) {
+    digits <- select(
+        as.data.frame(str_match_all(x, "(?=(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|([0-9]))")), !X1
+    ) %>% transmute(tracc = Reduce(coalesce, across(everything(), ~ replace(., . == "-1", NA_integer_))))
+    digits <- str_flatten(unlist(digits))
+    spelled_digits_replaced <- str_replace_all(digits, c(
+        "one" = "1",
+        "two" = "2",
+        "three" = "3",
+        "four" = "4",
+        "five" = "5",
+        "six" = "6",
+        "seven" = "7",
+        "eight" = "8",
+        "nine" = "9"
+    ))
+    basic_calibration_number(spelled_digits_replaced)
+}
+
+input <- readLines("input.txt")
+
+sum(basic_calibration_number(input))
+sum(unlist(map(input, calibration_number)))

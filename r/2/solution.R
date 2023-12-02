7b73fd6f14845aa5d9ed8672a13794d8961358ae
Eshan <eshansingh@gmail.com>
2023-12-02 04:23:40 -0500

    Replace NAs with 0s in the parsing for 2023-2

--- r/2/solution.R
+++ r/2/solution.R
@@ -6,7 +6,7 @@ games <- readLines("input.txt") |> str_split(";") |>
             str_match_all("(\\d+) (\\w+)") |>
             lapply(function(x) {
                 colors <- setNames(as.numeric(x[, 2]), x[, 3])
-                # We do this to ensure there are explicit NAs for colors not
+                # We do this to ensure there are explicit 0s for colors not
                 # specified which makes things clearer overall and slightly
                 # easier for part 2
                 all_colors <- c(
@@ -14,11 +14,12 @@ games <- readLines("input.txt") |> str_split(";") |>
                     green=unname(colors["green"]),
                     blue=unname(colors["blue"])
                 )
+                replace(all_colors, is.na(all_colors), 0)
             })
     })
 
 part1BagConfig <- c(red=12, green=13, blue=14)
-possibleGames <- sapply(games, \(game) all(sapply(game, \(round) all(round <= part1BagConfig, na.rm = TRUE))))
+possibleGames <- sapply(games, \(game) all(sapply(game, \(round) all(round <= part1BagConfig))))
 sum(which(possibleGames))
 
 minConfigs <- lapply(games, \(game) unlist(game) |> matrix(
@@ -27,5 +28,5 @@ minConfigs <- lapply(games, \(game) unlist(game) |> matrix(
     # We just reuse the same bag to avoid having to retype the column names, it
     # does not have any actual relevance to part 2 of course.
     dimnames=list(1:length(game), names(part1BagConfig))
-) |> apply(2, \(color) max(color, na.rm = TRUE)))
+) |> apply(2, max))
 sum(unlist(lapply(minConfigs, prod)))

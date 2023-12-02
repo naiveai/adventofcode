cd0832ef67734741938e286a4caba1e927d7f935
Eshan <eshansingh@gmail.com>
2023-12-01 20:08:55 -0500

    Use pipelines in R 2023-1

--- r/1/solution.R
+++ r/1/solution.R
@@ -2,32 +2,27 @@ library(tidyverse)
 
 basic_calibration_number <- function(x) {
     only_numeric <- gsub("[^0-9.-]", "", x)
-    two_digit_number_str <- paste(
+    paste(
         substr(only_numeric, 1, 1),
         substr(only_numeric, nchar(only_numeric), nchar(only_numeric)),
         sep=""
-    )
-    as.numeric(two_digit_number_str)
+    ) |> as.numeric()
 }
 
 calibration_number <- function(x) {
-    digits <- lapply(
-        str_match_all(x, "(?=(one|two|three|four|five|six|seven|eight|nine|[0-9]))"),
-        \(x) x[, 2]
-    )
-    digits <- str_flatten(unlist(digits))
-    spelled_digits_replaced <- str_replace_all(digits, c(
-        "one" = "1",
-        "two" = "2",
-        "three" = "3",
-        "four" = "4",
-        "five" = "5",
-        "six" = "6",
-        "seven" = "7",
-        "eight" = "8",
-        "nine" = "9"
-    ))
-    basic_calibration_number(spelled_digits_replaced)
+    str_match_all(x, "(?=(one|two|three|four|five|six|seven|eight|nine|[0-9]))") |>
+        lapply(\(x) x[, 2]) |> unlist() |> str_flatten() |>
+        str_replace_all(c(
+            "one" = "1",
+            "two" = "2",
+            "three" = "3",
+            "four" = "4",
+            "five" = "5",
+            "six" = "6",
+            "seven" = "7",
+            "eight" = "8",
+            "nine" = "9"
+        )) |> basic_calibration_number()
 }
 
 input <- readLines("input.txt")
